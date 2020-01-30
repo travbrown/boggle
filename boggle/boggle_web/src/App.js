f957f56008471de7149c4526f421c6b159ead459
Travis Brown <travjbrown98@gmail.com>
2020-01-29 19:48:27 -0800

    Start/Stop Button implemented
    - The button to Start/Stop works and hides the board when it should.

--- boggle/boggle_web/src/App.js
+++ boggle/boggle_web/src/App.js
@@ -1,15 +1,43 @@
-import React from 'react';
-import Boggle from './components/boggle'
+import React, { useState } from 'react';
+import Boggle from './components/boggle';
+import Button from '@material-ui/core/Button';
 import './App.css';
 
 
+
 function App() {
+
+  const [show, setShow] = useState(false);
+  const [ buttonText, setButtonText ] = useState('Start');
+
+  function Start(condition){
+    if(condition){
+      return <Boggle/>
+    }
+    return null;
+  }
+
+  function toggleVisibility(){
+    
+    if(show){
+      setButtonText('Start');
+    }else{
+      setButtonText('Stop');
+    }
+    
+    setShow(!show);
+  }
+
   return (
-    <div className={App.General}>
+    <div className="App">
       <header className="App-header">
         Boggle
+        <Button onClick ={() => toggleVisibility()}>
+          {buttonText}
+        </Button>
+
         <div>
-          <Boggle/>
+          {Start(show)}
         </div>
         Boggle mi Bomboclaat
       </header>
